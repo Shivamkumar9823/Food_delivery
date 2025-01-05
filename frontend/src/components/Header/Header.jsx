c9a54cb818e5bd7cf3c757f8c5dcbf5f1134d809
shivam kumar <shivamkumarsingh.sk.2020@gmail.com>
2025-01-05 14:51:16 +0530

    update image of header 2

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import "./Header.css"
 const Header = () => {
+  const headerStyle = {
+    height: '34vw', // Wrap units like 'vw' in quotes
+    margin: '30px auto',
+    background: "url('/Header2.jpg') no-repeat center center", // Use a string for the URL
+    backgroundSize: 'cover',
+    position: 'relative',
+    borderRadius: '25px',
+  };
+
+
   return (
-    <div className='header'> 
+    <div style={headerStyle} className='header'> 
         <div className="header-contents">
             <h2>Order your favourite food here</h2>
             <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary 
